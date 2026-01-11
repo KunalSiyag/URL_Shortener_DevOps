7ab40993ce37f4a2fd4d15f40d8c1b6a742c3279
Kunal Siyag <kunalsiyag@rediffmail.com>
2026-01-12 03:11:22 +0545

    Go boilerplate added

---
+++ cmd/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+	log.Println("Server started on port 8080")
+	err := http.ListenAndServe(":8080", mux)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
